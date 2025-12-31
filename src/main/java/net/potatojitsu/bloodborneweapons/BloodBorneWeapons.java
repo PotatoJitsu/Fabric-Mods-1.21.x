321a535de337f0dbfd4f2b284a84e14276cfac81
PotatoJitsu <danielpleso21@gmail.com>
2026-01-01 00:17:17 +0200

    added comment

---
+++ src/main/java/net/potatojitsu/bloodborneweapons/BloodBorneWeapons.java
@@ -0,0 +1,17 @@
+package net.potatojitsu.bloodborneweapons;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//very important
+public class BloodBorneWeapons implements ModInitializer {
+	public static final String MOD_ID = "bloodborneweapons";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+        
+	}
+}
---
+++ src/main/java/net/potatojitsu/bloodborneweapons/BloodBorneWeaponsClient.java
@@ -0,0 +1,10 @@
+package net.potatojitsu.bloodborneweapons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BloodBorneWeaponsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
