b76cc8508de778ff7bf5cdbc9cdfb4960f47b820
PotatoJitsu <danielpleso21@gmail.com>
2026-01-01 14:57:47 +0200

    added very important comment

--- src/main/java/net/potatojitsu/bloodborneweapons/BloodBorneWeapons.java
+++ src/main/java/net/potatojitsu/bloodborneweapons/BloodBorneWeapons.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//very important
+//very important comment
 public class BloodBorneWeapons implements ModInitializer {
 	public static final String MOD_ID = "bloodborneweapons";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
